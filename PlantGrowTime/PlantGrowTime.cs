a6f1e4e33ae7e445f1df116073936279a36186e0
Jc Dock <jcdock03@hotmail.com>
2021-04-14 21:18:35 +1200

    made logging less bloated

--- PlantGrowTime/PlantGrowTime.cs
+++ PlantGrowTime/PlantGrowTime.cs
@@ -38,7 +38,7 @@ namespace PlantGrowTime
             {
                 logger = Logger;
                
-                modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+                modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod (Requires game restart to re-enable)");
                 TurnipMultiplier = Config.Bind<float>("General", "TurnipMultiplier", 1f, "Set growtime multiplyer for Turnips");
                 SeedTurnipMultiplier = Config.Bind<float>("General", "SeedTurnipMultiplier", 1f, "Set growtime multiplyer for Seed Turnips");
                 CarrotMultiplier = Config.Bind<float>("General", "CarrotMultiplier", 1f, "Set growtime multiplyer for Carrots");
@@ -50,8 +50,10 @@ namespace PlantGrowTime
                 BeechMultiplier = Config.Bind<float>("General", "BeechMultiplier", 1f, "Set growtime multiplyer for Beech");
                 OtherMultiplier = Config.Bind<float>("General", "OtherMultiplier", 1f, "Set growtime multiplyer for all else");
                nexusID = Config.Bind<int>("General", "NexusID", 943, "Nexus mod ID for updates");
-             
-               
+
+                if (!modEnabled.Value)
+                    return;
+
                 harmony.PatchAll();
                 logger.LogInfo("Initialized Plant Time Mod");
                
@@ -65,76 +67,78 @@ namespace PlantGrowTime
             [HarmonyPatch(typeof(Plant), "Awake")]
             static class Plant_Awake_Patch
             {
-                static void Postfix(ref Plant __instance)
+                static void Prefix(ref Plant __instance)
                 {
                     string name = __instance.m_name;
                     if (name == "$piece_sapling_turnip")
                     {
                         __instance.m_growTime *= TurnipMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
 
                     }
                     else if (name == "$piece_sapling_seedturnip")
                     {
                         __instance.m_growTime *= SeedTurnipMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$piece_sapling_carrot")
                     {
                         __instance.m_growTime *= CarrotMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$piece_sapling_seedcarrot")
                     {
                        
                         __instance.m_growTime *= SeedCarrotMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$piece_sapling_barley")
                     {
                         __instance.m_growTime *= BarleyMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$piece_sapling_flax")
                     {
                         __instance.m_growTime *= FlaxMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$prop_fir_sapling")
                     {
                         __instance.m_growTime *= FirMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$prop_pine_sapling")
                     {
                        
                         __instance.m_growTime *= PineMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else if (name == "$prop_beech_sapling")
                     {
                         __instance.m_growTime *= BeechMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
                     }
                     else
                     {                
                         __instance.m_growTime *= OtherMultiplier.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                        logger.LogInfo($"Name:  {__instance.m_name} Grow Time: {__instance.m_growTime}");
+                       
 
                     }
+                    logger.LogInfo($"Name: {__instance.m_name} Grow Time: {__instance.m_growTime}");
                 }
             }
         }
+
     }
 
 }
