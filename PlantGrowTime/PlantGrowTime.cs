08f0414f6506ed43a53a99c4449c8c46597b9b97
Jc Dock <jcdock03@hotmail.com>
2021-05-03 10:19:50 +1200

    Refactor: change from using multiplyer to setting growtime directly

--- PlantGrowTime/PlantGrowTime.cs
+++ PlantGrowTime/PlantGrowTime.cs
@@ -23,16 +23,16 @@ namespace PlantGrowTime
             public static ConfigEntry<int> nexusID;
             public static ManualLogSource logger;
             public static ConfigEntry<bool> modEnabled;
-            private static ConfigEntry<float> TurnipMultiplier;
-            private static ConfigEntry<float> SeedTurnipMultiplier;
-            private static ConfigEntry<float> CarrotMultiplier;
-            private static ConfigEntry<float> SeedCarrotMultiplier;
-            private static ConfigEntry<float> FlaxMultiplier;
-            private static ConfigEntry<float> BarleyMultiplier;
-            private static ConfigEntry<float> FirMultiplier;
-            private static ConfigEntry<float> PineMultiplier;
-            private static ConfigEntry<float> BeechMultiplier;
-            private static ConfigEntry<float> OtherMultiplier;
+            private static ConfigEntry<float> TurnipGrowtime;
+            private static ConfigEntry<float> SeedTurnipGrowtime;
+            private static ConfigEntry<float> CarrotGrowtime;
+            private static ConfigEntry<float> SeedCarrotGrowtime;
+            private static ConfigEntry<float> FlaxGrowtime;
+            private static ConfigEntry<float> BarleyGrowtime;
+            private static ConfigEntry<float> FirGrowTime;
+            private static ConfigEntry<float> PineGrowTime;
+            private static ConfigEntry<float> BeechGrowTime;
+            private static ConfigEntry<float> OtherGrowTime;
             public static ConfigEntry<int> SeedTurnipDrop;
             public static ConfigEntry<int> TurnipDrop;
             public static ConfigEntry<int> SeedCarrotDrop;
@@ -48,22 +48,22 @@ namespace PlantGrowTime
                
                 modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod (Requires game restart to re-enable)");
                 nexusID = Config.Bind<int>("General", "NexusID", 943, "Nexus mod ID for updates (DO NOT MOIDFY THIS VALUE");
-                TurnipMultiplier = Config.Bind<float>("General", "Turnip Multiplier", 1f, "Set growtime multiplyer for Turnips");
-                SeedTurnipMultiplier = Config.Bind<float>("General", "Seed TurnipMultiplier", 1f, "Set growtime multiplyer for Seed Turnips");
-                CarrotMultiplier = Config.Bind<float>("General", "Carrot Multiplier", 1f, "Set growtime multiplyer for Carrots");
-                SeedCarrotMultiplier = Config.Bind<float>("General", "Seed CarrotMultiplier", 1f, "Set growtime multiplyer for Seed Carrots");
-                FlaxMultiplier = Config.Bind<float>("General", "Flax Multiplier", 1f, "Set growtime multiplyer for Flax");
-                BarleyMultiplier = Config.Bind<float>("General", "Barley Multiplier", 1f, "Set growtime multiplyer for Barley");
-                FirMultiplier = Config.Bind<float>("General", "Fir Multiplier", 1f, "Set growtime multiplyer for Fir");
-                PineMultiplier = Config.Bind<float>("General", "Pine Multiplier", 1f, "Set growtime multiplyer for Pine");
-                BeechMultiplier = Config.Bind<float>("General", "Beech Multiplier", 1f, "Set growtime multiplyer for Beech");
-                OtherMultiplier = Config.Bind<float>("General", "Other Multiplier", 1f, "Set growtime multiplyer for all else");
-                SeedTurnipDrop = Config.Bind<int>("General", "Seed Turnip Drop", 1, "Set amount of seed each Seed Turnip plant drops");
-                TurnipDrop = Config.Bind<int>("General", "Turnip Drop", 1, "Set amount of seed each Turnip plant drops");
-                SeedCarrotDrop = Config.Bind<int>("General", "Seed Carrot Drop", 1, "Set amount of seed each Seed Carrot plant drops");
-                CarrotDrop = Config.Bind<int>("General", "Carrot Drop", 1, "Set amount of seed each Carrot plant drops");
-                FlaxDrop = Config.Bind<int>("General", "Flax Drop", 1, "Set amount of seed each Flax plant drops");
-                BarleyDrop = Config.Bind<int>("General", "Barley Drop", 1, "Set amount of seed each Barley plant drops");
+                TurnipGrowtime = Config.Bind<float>("General", "Turnip GrowTime", 600f, "Set growtime in seconds for Turnips");
+                SeedTurnipGrowtime = Config.Bind<float>("General", "Seed TurnipGrowtime", 600f, "Set growtime in seconds for Seed Turnips");
+                CarrotGrowtime = Config.Bind<float>("General", "Carrot GrowTime", 600f, "Set growtime in seconds for Carrots");
+                SeedCarrotGrowtime = Config.Bind<float>("General", "Seed CarrotGrowtime", 600f, "Set growtime in seconds for Seed Carrots");
+                FlaxGrowtime = Config.Bind<float>("General", "Flax GrowTime", 600f, "Set growtime in seconds for Flax");
+                BarleyGrowtime = Config.Bind<float>("General", "Barley GrowTime", 600f, "Set growtime in seconds for Barley");
+                FirGrowTime = Config.Bind<float>("General", "Fir GrowTime", 1200f, "Set growtime in seconds for Fir");
+                PineGrowTime = Config.Bind<float>("General", "Pine GrowTime", 1200f, "Set growtime in seconds for Pine");
+                BeechGrowTime = Config.Bind<float>("General", "Beech GrowTime", 1200f, "Set growtime in seconds for Beech");
+                OtherGrowTime = Config.Bind<float>("General", "Other GrowTime", 600f, "Set growtime in seconds for all else");
+                SeedTurnipDrop = Config.Bind<int>("General", "Seed Turnip Drop", 3, "Set amount of seed each Seed Turnip plant drops");
+                TurnipDrop = Config.Bind<int>("General", "Turnip Drop", 2, "Set amount of seed each Turnip plant drops");
+                SeedCarrotDrop = Config.Bind<int>("General", "Seed Carrot Drop", 3, "Set amount of seed each Seed Carrot plant drops");
+                CarrotDrop = Config.Bind<int>("General", "Carrot Drop", 2, "Set amount of seed each Carrot plant drops");
+                FlaxDrop = Config.Bind<int>("General", "Flax Drop", 2, "Set amount of seed each Flax plant drops");
+                BarleyDrop = Config.Bind<int>("General", "Barley Drop", 2, "Set amount of seed each Barley plant drops");
                 if (!modEnabled.Value)
                     return;
 
@@ -99,63 +99,63 @@ namespace PlantGrowTime
                     string name = __instance.m_name;
                     if (name == "$piece_sapling_turnip")
                     {
-                        __instance.m_growTime *= TurnipMultiplier.Value;
+                        __instance.m_growTime = TurnipGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
 
                     }
                     else if (name == "$piece_sapling_seedturnip")
                     {
-                        __instance.m_growTime *= SeedTurnipMultiplier.Value;
+                        __instance.m_growTime = SeedTurnipGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$piece_sapling_carrot")
                     {
-                        __instance.m_growTime *= CarrotMultiplier.Value;
+                        __instance.m_growTime = CarrotGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$piece_sapling_seedcarrot")
                     {
                        
-                        __instance.m_growTime *= SeedCarrotMultiplier.Value;
+                        __instance.m_growTime = SeedCarrotGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$piece_sapling_barley")
                     {
-                        __instance.m_growTime *= BarleyMultiplier.Value;
+                        __instance.m_growTime = BarleyGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$piece_sapling_flax")
                     {
-                        __instance.m_growTime *= FlaxMultiplier.Value;
+                        __instance.m_growTime = FlaxGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$prop_fir_sapling")
                     {
-                        __instance.m_growTime *= FirMultiplier.Value;
+                        __instance.m_growTime = FirGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$prop_pine_sapling")
                     {
                        
-                        __instance.m_growTime *= PineMultiplier.Value;
+                        __instance.m_growTime = PineGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else if (name == "$prop_beech_sapling")
                     {
-                        __instance.m_growTime *= BeechMultiplier.Value;
+                        __instance.m_growTime = BeechGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
                     }
                     else
                     {                
-                        __instance.m_growTime *= OtherMultiplier.Value;
+                        __instance.m_growTime = OtherGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
                        
 
