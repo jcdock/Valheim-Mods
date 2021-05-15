753eeaabc0f64d64151b577d5875cf29bb4ad917
Jc Dock <jcdock03@hotmail.com>
2021-05-15 21:03:06 +1200

    fix errors caused by merg

--- PlantGrowTime/PlantGrowTime.cs
+++ PlantGrowTime/PlantGrowTime.cs
@@ -19,7 +19,7 @@ namespace PlantGrowTime
         public class PlantGrowTime : BaseUnityPlugin
         {
             private readonly Harmony harmony = new Harmony("Plant Grow Time");
-            
+
             public static ConfigEntry<int> nexusID;
             public static ManualLogSource logger;
             public static ConfigEntry<bool> modEnabled;
@@ -44,18 +44,18 @@ namespace PlantGrowTime
             public static ConfigEntry<int> CarrotDrop;
             public static ConfigEntry<int> FlaxDrop;
             public static ConfigEntry<int> BarleyDrop;
-            
-            
+
+
 
             void Awake()
             {
                 logger = Logger;
 
-                
+
                 modEnabled = Config.Bind<bool>("Mod Control", "Mod Enabled", true, "Enable this mod (Requires game restart to re-enable)");
                 GrowRateEnabled = Config.Bind<bool>("Mod Control", "Grow Rate Change Enabled", true, "Enable Grow Rate Change");
                 dropRateEnabled = Config.Bind<bool>("Mod Control", "Drop Rate Change Enabled", true, "Enable Drop Rate Change");
-                hoverTextEnabled = Config.Bind<bool>("Mod Control","Hover Text Enabled",false,"Enable Hover Text");
+                hoverTextEnabled = Config.Bind<bool>("Mod Control", "Hover Text Enabled", false, "Enable Hover Text");
                 nexusID = Config.Bind<int>("Mod Control", "NexusID", 943, "Nexus mod ID for updates (DO NOT MOIDFY THIS VALUE)");
                 TurnipGrowtime = Config.Bind<float>("General", "Turnip GrowTime", 600f, "Set growtime in seconds for Turnips");
                 SeedTurnipGrowtime = Config.Bind<float>("General", "Seed TurnipGrowtime", 600f, "Set growtime in seconds for Seed Turnips");
@@ -79,7 +79,7 @@ namespace PlantGrowTime
 
                 harmony.PatchAll();
                 logger.LogInfo("Initialized Plant Time Mod");
-               
+
             }
 
             void OnDestroy()
@@ -124,57 +124,57 @@ namespace PlantGrowTime
                     {
                         __instance.m_growTime = SeedTurnipGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$piece_sapling_carrot")
                     {
                         __instance.m_growTime = CarrotGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$piece_sapling_seedcarrot")
                     {
-                       
+
                         __instance.m_growTime = SeedCarrotGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$piece_sapling_barley")
                     {
                         __instance.m_growTime = BarleyGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$piece_sapling_flax")
                     {
                         __instance.m_growTime = FlaxGrowtime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$prop_fir_sapling")
                     {
                         __instance.m_growTime = FirGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$prop_pine_sapling")
                     {
-                       
+
                         __instance.m_growTime = PineGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else if (name == "$prop_beech_sapling")
                     {
                         __instance.m_growTime = BeechGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
                     }
                     else
-                    {                
+                    {
                         __instance.m_growTime = OtherGrowTime.Value;
                         __instance.m_growTimeMax = __instance.m_growTime;
-                       
+
 
 
                     }
@@ -225,45 +225,18 @@ namespace PlantGrowTime
 
                 }
             }
+        
+        
+        
+        
+        }
+    }
+}
+            
+            
 
 
-                    logger.LogInfo($"Name: {__instance.m_name} Grow Time: {__instance.m_growTime}");
-                }
-            }
+                   
+          
 
-            [HarmonyPatch(typeof(Pickable), "Awake")]
-            static class Pickable_Awake_Patch
-            {
-                static void Postfix(ref Pickable __instance)
-                {
-                    
-                    string name = __instance.GetHoverName();
-                    if (name == "$item_turnipseeds")
-                    {
-                        __instance.m_amount = SeedTurnipDrop.Value;
-                        logger.LogInfo($"Set: {name} to drop: {SeedTurnipDrop.Value}");
-                    }
-                    if (name == "$item_turnip")
-                    {
-                        __instance.m_amount = TurnipDrop.Value;
-                        logger.LogInfo($"Set: {name} to drop: {TurnipDrop.Value}");
-                    }
-                    if (name == "$item_carrotseeds")
-                    {
-                        __instance.m_amount = SeedTurnipDrop.Value;
-                        logger.LogInfo($"Set: {name} to drop: {SeedCarrotDrop.Value}");
-                    }
-                    if (name == "$item_barley")
-                    {
-                        __instance.m_amount = BarleyDrop.Value;
-                        logger.LogInfo($"Set: {name} to drop: {BarleyDrop.Value}");
-                    }
-                    if (name == "$item_flax")
-                    {
-                        __instance.m_amount = FlaxDrop.Value;
-                        logger.LogInfo($"Set: {name} to drop: {FlaxDrop.Value}");
-                    }
-
-                }
-            }
 
